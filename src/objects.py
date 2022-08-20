3760099ede304ee44cbf806ff9a71b0d72f44a4b
Blu3Line <celal.erkuss@gmail.com>
2022-08-20 23:45:26 +0300

    token yeri degisti

--- src/objects.py
+++ src/objects.py
@@ -3,6 +3,5 @@ from telebot.async_telebot import AsyncTeleBot
 import os
 import json
 
-with open("src\config.json") as read:
-    cfg = json.load(read)
-bot = AsyncTeleBot(cfg["api-key"])
+api_key = ""
+bot = AsyncTeleBot(api_key)
