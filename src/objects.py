f9faccf394d61b78488b1beb59adc5dbf1e493c7
Blu3Line <celal.erkuss@gmail.com>
2022-08-22 17:43:08 +0300

    heroku stil cfgvar

--- src/objects.py
+++ src/objects.py
@@ -1,5 +1,4 @@
 from telebot.async_telebot import AsyncTeleBot
-from boto.s3.connection import S3Connection
 
 import os
 import json
@@ -11,7 +10,4 @@ cfgpath = os.path.join(dirpath, 'config.json')
 with open(cfgpath) as f:
     cfg = json.load(f)
 
-#herokudan api key almak için
-s3 = S3Connection(os.environ['api-key'])
-
-bot = AsyncTeleBot(s3)
+bot = AsyncTeleBot(os.getenv("api-key"))
